78aa9684c1c56389691dcc0bc92a5dfa070cf9e7
yuwat <yuwat@mfec.co.th>
2019-07-13 14:02:08 +0700

    network first for manifest.json

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-// Use a stale-while-revalidate strategy for all requests.
+// manifest must always fresh
+workbox.routing.registerRoute(
+  /manifest\.json$/,
+  new workbox.strategies.NetworkFirst()
+);
+
+// Use a stale-while-revalidate strategy for all other requests.
 workbox.routing.setDefaultHandler(
   new workbox.strategies.StaleWhileRevalidate()
 );
