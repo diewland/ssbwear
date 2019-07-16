01e4e6af33caa651c3c8039f65cb46f1f090af41
yuwat <yuwat@mfec.co.th>
2019-07-16 23:51:07 +0700

    revise service-worker

--- sw.js
+++ sw.js
@@ -1,11 +1,21 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-// find from network first
+// json files - network first
 // - manifest.json
 // - .well-known/assetlinks.json
 workbox.routing.registerRoute(
-  /(manifest|assetlinks)\.json/,
-  new workbox.strategies.NetworkFirst()
+  /(manifest|assetlinks)\.json$/,
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'json',
+  })
+);
+
+// images - cache first
+workbox.routing.registerRoute(
+  /\.(jpg|gif|png)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+  })
 );
 
 // Use a stale-while-revalidate strategy for all other requests.
